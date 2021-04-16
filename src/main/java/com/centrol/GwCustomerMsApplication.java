b3c645054824ad3066c6eea3e6f500919866ab27
abennani <adil.bennani.ma@gmail.com>
2021-04-16 15:42:49 +0200

    First commit

---
+++ src/main/java/com/centrol/GwCustomerMsApplication.java
@@ -0,0 +1,34 @@
+package com.centrol;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.centrol.dao.CustomerRepository;
+import com.centrol.model.Customer;
+
+@SpringBootApplication
+public class GwCustomerMsApplication {
+
+	Logger logger = LoggerFactory.getLogger(GwCustomerMsApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwCustomerMsApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository) {
+		return args -> {
+			customerRepository.save(new Customer(null, "Bennani", "bennani@gmail.com"));
+			customerRepository.save(new Customer(null, "Gregory", "gregory@gmail.com"));
+			customerRepository.save(new Customer(null, "Maxwell", "maxwell@gmail.com"));
+			customerRepository.findAll().forEach(c -> {
+				logger.trace(c.getName() + " " + c.getEmail());
+			});
+		};
+	}
+
+}
---
+++ src/test/java/com/centrol/GwCustomerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwCustomerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
